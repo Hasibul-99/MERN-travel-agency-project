ccaa42621b3c7a07284ef6e75a646bd103a8421d
hasibul-99 <hasibul_uap@outlook.com>
2020-07-21 10:09:52 +0600

    request and body

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
 
-// @route   GET api/users
-// @dosc    test route
+// @route   POST api/users
+// @dosc    Register route
 // @access  public
-router.get('/', (req, res) => res.send("User route"));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+],(req, res) => {
+    const errors = validationResult(req);
+
+    console.log("errors", errors);
+    
+    
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    res.send("User route")
+});
 
 module.exports = router;
