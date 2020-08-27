cc0423acec55a6ad97470ab9c8c5e9eb27be4604
Hasibul Hasan <hasibul_uap@outlook.com>
2020-08-27 13:35:49 +0600

    user image upload

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,7 +1,13 @@
+const path = require('path');
 const express = require("express");
 const { check, validationResult } = require("express-validator");
 const User = require('../../models/Users');
+const auth  = require('../../middleware/auth');
 const router = express.Router();
+
+const config  = require("config");
+const filePath = config.get('userProfileImage');
+const maxFieleSize = config.get('maxFileSize');
 
 // @route   POST api/users
 // @dosc    Register route
@@ -38,6 +44,55 @@ router.post('/', [
     } catch(err) {
         console.error("err", err.message);
         res.status(500).send("Server error");
+    }
+});
+
+
+// @route   PUT api/users/:id/photo
+// @dosc    put user photo
+// @access  private
+router.put('/:id/photo', auth, async (req, res) => {
+    console.log("hello");
+    
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        };
+
+        if (!req.files) {
+            return res.status(400).send('Please upload a file');
+        };
+
+        const file = req.files.file;
+
+        if (!file.mimetype.startsWith('image')) {
+            return res.status(400).send('Please upload an image file');
+        };
+
+        if (file.size > maxFieleSize) {
+            return res.status(400).send(`Please upload an image less than 1000000`);
+        };
+
+        // Create custom filename
+        file.name = `photo_${user._id}-${path.parse(file.name).ext}`;
+
+        file.mv(`${filePath}/${file.name}`, async err => {
+            
+            if (err) {
+                return res.status(500).send(`Problem with file upload`);
+            }
+            let data = await User.findByIdAndUpdate(req.params.id, {$set: {avatar: `users/${file.name}`}}, {new: true});
+
+            res.status(200).json({
+                success: true,
+                data: data
+            });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
     }
 });
 
